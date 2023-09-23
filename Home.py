b88a96da721fd141d4461b75da2ed9cc7986f02e
JimJamOfTheHam <143989620+JimJamOfTheHam@users.noreply.github.com>
2023-09-23 23:08:05 +0930

    working out if statement to work on the other if statement

--- Home.py
+++ Home.py
@@ -8,7 +8,7 @@ def index():
 
 @app.route('/questions')
 def questions():
-    g.beforethequestion = f"Aussie kids are"
+    g.beforethequestion = f"Question 1. Aussie kids are"
     g.afterthequestion = f"kids. "
     return render_template('Fill_In_The_Blanks.html')
 
@@ -16,19 +16,22 @@ def questions():
 def get_postanwer():
     x = request.form['message']
     y = request.form['number']
-    if y == '1':
-        g.beforethequestion = f"Aussie kids are"
+    if y == '0':
+        g.beforethequestion = f'Question 1. Aussie kids are'
+        g.afterthequestion = f'kids.'
+        
+    elif y == '1':
+        g.beforethequestion = f'Question 2: Just'
+        g.afterthequestion = f'It.'
+        if x == "Do":
+            g.beforethequestion = f'Question 3: Snap'
+            g.afterthequestion = f'Pop.'
+
+    else:
+        g.answerfeedback = f"You Haven't Filled In The Question Or You Haven't Filled In The Question Numer."
+        g.beforethequestion = f"Question 1. Aussie kids are"
         g.afterthequestion = f"kids. "
-        if x == 'weetbix':
-            g.answerfeedback = f'Correct, Try This One.'
-        else:
-            g.answerfeedback = f'Incorrect Try Again.'
-
-    elif y=='2':
-        print('Yes')
-
-    print ('THIS WORKS', y)
-
+    
     return render_template('Fill_In_The_Blanks.html')
 
 app.run(debug=True, port=2007)
