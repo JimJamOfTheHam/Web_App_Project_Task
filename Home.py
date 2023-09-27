31dd4687929edc0e40cfe59ea99912307425d1af
JimJamOfTheHam <143989620+JimJamOfTheHam@users.noreply.github.com>
2023-09-27 10:43:07 +0930

    Finishing the first game

--- Home.py
+++ Home.py
@@ -8,29 +8,41 @@ def index():
 
 @app.route('/questions')
 def questions():
-    g.beforethequestion = f"Question 1. Aussie kids are"
-    g.afterthequestion = f"kids. "
+    g.beforethequestion = f""
+    g.afterthequestion = f""
     return render_template('Fill_In_The_Blanks.html')
 
 @app.post('/postanswer')
 def get_postanwer():
     x = request.form['message']
-    y = request.form['number']
-    if y == '0':
+    if x == 'game 1' and 'games 1':
         g.beforethequestion = f'Question 1. Aussie kids are'
         g.afterthequestion = f'kids.'
-        
-    elif y == '1':
-        g.beforethequestion = f'Question 2: Just'
-        g.afterthequestion = f'It.'
-        if x == "Do":
-            g.beforethequestion = f'Question 3: Snap'
-            g.afterthequestion = f'Pop.'
+
+    elif x == 'weetbix':
+        g.answerfeedback = f'Correct'
+        g.beforethequestion = f'Question 2: just'
+        g.afterthequestion = f'it.'
+
+    elif x == 'do':
+        g.answerfeedback = f'Correct'
+        g.beforethequestion = f'Question 3: Snap'
+        g.afterthequestion = f'Pop!.'
+
+    elif x == 'crackel':
+        g.answerfeedback = f'Correct'
+        g.afterthequestion = f"licken' good !."
+
+    elif x == 'finger':
+        g.answerfeedback = f'Correct'
+        g.beforethequestion = f'Question 4: Snap'
+        g.afterthequestion = f'Pop!.'
+
 
     else:
-        g.answerfeedback = f"You Haven't Filled In The Question Or You Haven't Filled In The Question Numer."
-        g.beforethequestion = f"Question 1. Aussie kids are"
-        g.afterthequestion = f"kids. "
+        g.answerfeedback = f"Inncorect else"
+        g.beforethequestion = f""
+        g.afterthequestion = f""
     
     return render_template('Fill_In_The_Blanks.html')
 
