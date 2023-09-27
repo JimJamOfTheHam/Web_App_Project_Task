4bb9f386567d3cea89c67a35890da3d662a24fc4
JimJamOfTheHam <143989620+JimJamOfTheHam@users.noreply.github.com>
2023-09-27 19:54:37 +0930

    Finish Fill in the blank and started on True or False

--- Home.py
+++ Home.py
@@ -6,16 +6,16 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/questions')
-def questions():
+@app.route('/questionsfill')
+def questionsfill():
     g.beforethequestion = f""
     g.afterthequestion = f""
     return render_template('Fill_In_The_Blanks.html')
 
-@app.post('/postanswer')
-def get_postanwer():
+@app.post('/postanswerfill')
+def get_postanswer():
     x = request.form['message']
-    if x == 'game 1' and 'games 1':
+    if x == 'game 1':
         g.beforethequestion = f'Question 1. Aussie kids are'
         g.afterthequestion = f'kids.'
 
@@ -35,15 +35,37 @@ def get_postanwer():
 
     elif x == 'finger':
         g.answerfeedback = f'Correct'
-        g.beforethequestion = f'Question 4: Snap'
-        g.afterthequestion = f'Pop!.'
+        g.beforethequestion = f"Question 4: I’m"
+        g.afterthequestion = f'It!.'
 
+    elif x =='loving':
+        g.answerfeedback = f'Correct'
+        g.beforethequestion = f"Question 5:"
+        g.afterthequestion = f'Gives You Wings'
+
+    elif x == 'RedBull':
+        g.answerfeedback = f'Correct'
+        g.beforethequestion = f"Well Done You Have Finished The Slogans"
+        g.afterthequestion = f'Now Write game 2.'
 
     else:
-        g.answerfeedback = f"Inncorect else"
+        g.answerfeedback = f"Inncorect Try Again"
         g.beforethequestion = f""
         g.afterthequestion = f""
     
     return render_template('Fill_In_The_Blanks.html')
+
+@app.route('/questiontrue')
+def questiontrue():
+    g.beforethequestion = f''
+    g.afterthequestion = f''
+    return render_template('trueorfalse')
+
+@app.post('/postanswertrue')
+def get_postanswer():
+    x = request.form['message']
+    if x == 'game 1':
+        g.beforethequestion = f'True or False'
+        g.afterthequestiom = f'False or True'
 
 app.run(debug=True, port=2007)
