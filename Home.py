024d93f1867b5657354bf263aba64b9c8a677580
JimJamOfTheHam <143989620+JimJamOfTheHam@users.noreply.github.com>
2023-09-28 14:48:49 +0930

    finished part 2 of fill in the blanks

--- Home.py
+++ Home.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
-
+#Fill In The Blanks
 @app.route('/questionsfill')
 def questionsfill():
     g.beforethequestion = f""
@@ -13,7 +13,7 @@ def questionsfill():
     return render_template('Fill_In_The_Blanks.html')
 
 @app.post('/postanswerfill')
-def get_postanswer():
+def get_postanswerfill():
     x = request.form['message']
     if x == 'game 1':
         g.beforethequestion = f'Question 1. Aussie kids are'
@@ -43,10 +43,46 @@ def get_postanswer():
         g.beforethequestion = f"Question 5:"
         g.afterthequestion = f'Gives You Wings'
 
-    elif x == 'RedBull':
+    elif x == 'redbull':
         g.answerfeedback = f'Correct'
         g.beforethequestion = f"Well Done You Have Finished The Slogans"
         g.afterthequestion = f'Now Write game 2.'
+
+    elif x =='game 2':
+        g.beforethequestion = f'It is what it'
+
+    elif x == 'is':
+        g.answerfeedback = f'Correct'
+        g.beforethequestion = f'Whatever floats your'
+
+    elif x == 'boat':
+        g.answerfeedback = f'Correct'
+        g.beforethequestion = f'Better late then'
+    
+    elif x == 'never':
+        g.answerfeedback = f'Correct'
+        g.beforethequestion = f'Easier '
+        g.afterthequestion = f'then done'
+
+    elif x == 'said':
+        g.answerfeedback = f'Correct'
+        g.afterthequestion = f'to my ears'
+
+    elif x == 'music':
+        g.answerfeedback = f'Correct'
+        g.beforethequestion = f"That’s what"
+        g.afterthequestion = f'said'
+
+    elif x == 'she':
+        g.answerfeedback = f'Correct'
+        g.beforethequestion = f'Don’t bit the'
+        g.afterthequestion = f'that feeds you'
+
+    elif x == 'hand':
+        g.answerfeedback = f'Correct'
+        g.beforethequestion = f"Well Done You Have Finished The Sayings/Phrases"
+        g.afterthequestion = f'Now Write game 3.'
+
 
     else:
         g.answerfeedback = f"Inncorect Try Again"
@@ -54,18 +90,22 @@ def get_postanswer():
         g.afterthequestion = f""
     
     return render_template('Fill_In_The_Blanks.html')
-
+#True or False
 @app.route('/questiontrue')
 def questiontrue():
     g.beforethequestion = f''
     g.afterthequestion = f''
-    return render_template('trueorfalse')
+    return render_template('trueorfalse.html')
 
 @app.post('/postanswertrue')
-def get_postanswer():
+def get_postanswertrue():
     x = request.form['message']
     if x == 'game 1':
         g.beforethequestion = f'True or False'
-        g.afterthequestiom = f'False or True'
+        g.afterthequestion = f'False or True'
+
+    else:
+        g.beforethequestion = f''
+    return render_template('trueorfalse.html')
 
 app.run(debug=True, port=2007)
