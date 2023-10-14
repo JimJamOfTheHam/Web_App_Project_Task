3d8319779bae50aaa6e89d04b8e347a27b990d7b
BAILEYPENNO <143990003+BAILEYPENNO@users.noreply.github.com>
2023-10-15 09:29:29 +1030

    fixed error working from this point

--- chat.py
+++ chat.py
@@ -2,12 +2,14 @@ from flask import *
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def homepage():
     return render_template('homepage.html')
 
-@app.route('/create_post', methods=['post'])
-def create_post():
+
+@app.route('/createpost', methods=['post'])
+def createpost():
     return "SUCCESS"
 
-app.run(debug=True)
+app.run(debug=True, port=2005)
