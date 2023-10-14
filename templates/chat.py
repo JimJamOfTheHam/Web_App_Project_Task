f79b6763ae56778697160612f495307a0b0cb7b5
BAILEYPENNO <143990003+BAILEYPENNO@users.noreply.github.com>
2023-10-14 19:48:57 +1030

    alot of updates to chat room still experincing unknowen error?

--- templates/chat.py
+++ templates/chat.py
@@ -6,4 +6,8 @@ app = Flask(__name__)
 def homepage():
     return render_template('homepage.html')
 
+@app.route('/create_post', methods=['post'])
+def create_post():
+    return "SUCCESS"
+
 app.run(debug=True)
