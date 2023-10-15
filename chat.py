3411a12046f277438ba3dd9f86dabb5d52db65f3
BAILEYPENNO <143990003+BAILEYPENNO@users.noreply.github.com>
2023-10-15 14:23:42 +1030

    working basic chatromm finished

--- chat.py
+++ chat.py
@@ -1,15 +1,39 @@
 from flask import *
+import dataset
 
 app = Flask(__name__)
-
+db = dataset.connect('sqlite:///chatroom.db')
+app.secret_key = 'asdfghjkl'
 
 @app.route("/")
 def homepage():
-    return render_template('homepage.html')
+    if 'username' not in session:
+        return redirect ('/login')
+    return render_template('homepage.html' , posts=db['posts'] )
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    del session ['username']
+    return redirect ('/')
+
+@app.route('/login_post', methods=['post'])
+def login_post():
+   session['username'] = (request.form['username'])
+   return redirect('/')
 
 
 @app.route('/createpost', methods=['post'])
 def createpost():
-    return "SUCCESS"
+    post_dictionary = {
+        'message' : request.form ['message'],
+        'username' :session['username']
+    }
+    db['posts'].insert(post_dictionary) 
+
+    return redirect('/') 
 
 app.run(debug=True, port=2005)
